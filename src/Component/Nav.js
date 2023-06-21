d8e52fd3715e0b5c06c167ceab8ed1da1160754d
whtn0000 <7563sumin@gmail.com>
2023-06-21 16:52:15 +0900

    Update Nav.js

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -7,21 +7,23 @@ import Statistics from './Statistics';
 import PopPost from './PopPost';
 const Nav = () => {
     return (
-        <div className='menu_bar'>
+        <div>
+         <div className='menu_bar'>
             <img id='logo' alt="이미지를 불러오는데 실패했습니다!" src='/zendeelogo.svg'></img>
             <div id ='menu'>
             <Link to='pop_post'>인기게시물</Link>
             <Link to='declaration'>신고</Link>
             <Link to='statistics' >통계</Link>
             </div>
-            <div>
+         </div>   
+         <div>
             <Routes>
                 <Route path='/pop_post'element={<PopPost/>}></Route>
                 <Route path='/declaration' element={<Declaration/>}></Route>
                 <Route path='/statistics' element={<Statistics/>}></Route>
                 
             </Routes>
-            </div>
+            </div>    
         </div>
     );
 };
